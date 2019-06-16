86f26dca06f6ba5d37f664cc5f0b7ba6bc41a6c2
Fidel Morales <fidel.morales@taskeasy.com>
2019-06-16 14:44:16 -0600

    Add put.py

---
+++ services/candidateRolesUrl/put.py
@@ -0,0 +1,45 @@
+import sys
+import logging
+import pymysql
+import os
+
+# Read configuration from enviroment variables.
+rds_host = os.environ['db_endpoint']
+name = os.environ['db_username']
+password = os.environ['db_password']
+db_name = os.environ['db_name']
+port = os.environ['db_port']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
+
+def lambda_handler(event, context):
+
+    positionId = event['positionId']
+
+    query = f'''
+        UPDATE POSITION
+        SET
+        POSITION = '{position}'
+        WHERE POSITION_ID = {positionId}
+        '''
+
+    with conn.cursor() as cur:
+        cur.execute(query)
+        conn.commit()
+
+    response = {
+        "statusCode": 200,
+        "PARTY": positionId,
+        "message": "Position succesfully updated",
+    }
+    return response
+
